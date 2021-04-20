56457b0d43c12756e0fe13c2bf339c03b7f9d187
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2021-04-20 12:48:40 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-wget https://opendata.ecdc.europa.eu/covid19/casedistribution/csv -O covid-eucdc.csv
+# wget https://opendata.ecdc.europa.eu/covid19/casedistribution/csv -O covid-eucdc.csv
+wget https://opendata.ecdc.europa.eu/covid19/nationalcasedeath_eueea_daily_ei/csv/data.csv -O covid-eucdc.csv
 
 mkdir -p build
 python3 build.py
